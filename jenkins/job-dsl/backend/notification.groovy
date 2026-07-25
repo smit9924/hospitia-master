c4e8505aebb2e3ce55490970e9d803b7b51c8bd2
smit patel <smit.dpatel9924@gmail.com>
2026-07-25 19:58:42 +0000

    feat: add notification service job configuration to Jenkins Job DSL

---
+++ jenkins/job-dsl/backend/notification.groovy
@@ -0,0 +1,56 @@
+final String REPOSITORY_URL = 'https://github.com/smit9924/angular-fastapi-template-backend'
+final String CREDENTIALS_ID = 'github-token'
+final String JENKINSFILE = 'services/notification/jenkins/Jenkinsfile'
+
+final List<String> BRANCHES = [
+    'main',
+    'feature/notification-service',
+]
+
+pipelineJob('Backend/notification') {
+
+    displayName('Notification')
+
+    description(
+        '''
+        Builds and deploys the Notification service.
+
+        This job is managed by Job DSL.
+        Do not edit it manually from the Jenkins UI.
+        '''.stripIndent()
+    )
+
+    logRotator {
+        daysToKeep(1)
+        numToKeep(2)
+    }
+
+    parameters {
+        choiceParam(
+            'SOURCE_BRANCH',
+            BRANCHES,
+            'Select the source branch to build.'
+        )
+    }
+
+    definition {
+        cpsScm {
+
+            scm {
+                git {
+
+                    remote {
+                        url(REPOSITORY_URL)
+                        credentials(CREDENTIALS_ID)
+                    }
+
+                    branch('*/${SOURCE_BRANCH}')
+                }
+            }
+
+            scriptPath(JENKINSFILE)
+
+            lightweight(true)
+        }
+    }
+}
