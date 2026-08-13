e6d0fd609301a55d3dc7cd24676841523587392a
smit9924 <smit.dpatel9924@gmail.com>
2026-08-13 18:01:45 +0000

    feat: add Booking database and Jenkins pipeline job for Booking service

---
+++ jenkins/job-dsl/backend/booking.groovy
@@ -0,0 +1,55 @@
+final String REPOSITORY_URL = 'https://github.com/smit9924/angular-fastapi-template-backend'
+final String CREDENTIALS_ID = 'github-token'
+final String JENKINSFILE = 'services/booking/jenkins/Jenkinsfile'
+
+final List<String> BRANCHES = [
+    'main',
+]
+
+pipelineJob('Backend/booking') {
+
+    displayName('Booking')
+
+    description(
+        '''
+        Builds and deploys the Booking service.
+
+        This job is managed by Job DSL.
+        Do not edit it manually from the Jenkins UI.
+        '''.stripIndent()
+    )
+
+    logRotator {
+        daysToKeep(1)
+        numToKeep(2)
+    }
+
+    parameters {
+        choiceParam(
+            'SOURCE_BRANCH',
+            BRANCHES,
+            'Select the source branch to build.'
+        )
+    }
+
+    definition {
+        cpsScm {
+
+            scm {
+                git {
+
+                    remote {
+                        url(REPOSITORY_URL)
+                        credentials(CREDENTIALS_ID)
+                    }
+
+                    branch('*/${SOURCE_BRANCH}')
+                }
+            }
+
+            scriptPath(JENKINSFILE)
+
+            lightweight(true)
+        }
+    }
+}
